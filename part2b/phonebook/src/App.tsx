4b86aa4dcec3ac22f3a6b261ba7fc4ada3162d3d
Jon Carter <graphicnapkin>
2022-06-12 11:22:27 -0400

    2.11

--- part2b/phonebook/src/App.tsx
+++ part2b/phonebook/src/App.tsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ContactType } from './types'
 import Filter from './components/Search'
 import PersonForm from './components/Form'
 import Numbers from './components/Numbers'
-
+import Persons from './components/api'
 const App = () => {
     const [persons, setPersons] = useState<ContactType[]>([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+        { name: '', number: '', id: NaN },
     ])
-
     const [filter, setFilter] = useState<string>('')
+
+    useEffect(() => {
+        Persons.then((res) => setPersons(res.data as ContactType[]))
+    }, [Persons])
 
     return (
         <div>
---
+++ part2b/phonebook/src/components/api.tsx
@@ -0,0 +1,4 @@
+import axios from 'axios'
+
+const response = axios.get('http://localhost:3001/persons')
+export default response
