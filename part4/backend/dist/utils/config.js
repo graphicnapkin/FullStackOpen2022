dd0b0cbb29b66eb334c031e0d773f80abc08f15e
graphicnapkin <graphicnapkin@gmail.com>
2022-06-22 23:46:13 -0400

    added supertest and updated environment variable for test and dev mongo db

--- part4/backend/dist/utils/config.js
+++ part4/backend/dist/utils/config.js
@@ -2,7 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require('dotenv').config();
 var PORT = process.env.PORT || 3001;
-var MONGODB_URI = process.env.MONGODB_URI || '';
+var MONGODB_URI = process.env.MONGODB_URI;
+if (process.env.NODE_ENV === 'test')
+    MONGODB_URI = process.env.TEST_MONGODB_URI || '';
+if (process.env.NODE_ENV === 'development')
+    MONGODB_URI = process.env.DEV_MONGODB_URI || '';
 exports.default = {
     MONGODB_URI: MONGODB_URI,
     PORT: PORT,
